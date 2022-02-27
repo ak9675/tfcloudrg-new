6248028705b9e51128ec07a8844a9f65832a9c45
ak9675 <98090519+ak9675@users.noreply.github.com>
2022-02-27 09:43:18 +0000

    Update rg.tf

--- rg.tf
+++ rg.tf
@@ -5,3 +5,7 @@ resource "azurerm_resource_group" "example" {
   name     = "tfcloudrg"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "example" {
+  name     = "tfcloudrg1"
+  location = "West Europe"
+}
